5734f881aa8d84433ba8c43820d4b4693c5b30e1
Vishal Tayal <vishal.tayal@nokia.com>
2018-03-28 10:17:53 +0530

    Made Changes in Koinex_GUI Class and Display_data Class.

--- Display_data.py
+++ Display_data.py
@@ -8,16 +8,19 @@ import Plot_graph
 import multiprocessing
 
 def backend():
+    print "Entering BackEnd"
     time=Fetch_time.Fetch_time()
     Koinex = {}
-    while True:
-        curr_time = time.currentTime()
-        url1=Koinex_Get_data.Getdata("https://koinex.in/api/ticker")
-        url1.getdatafromurl()
-        finaldict={curr_time:url1.convertdatafromurl().get('prices')}
-        Koinex.update(finaldict)
-        df = pd.read_json(json.dumps(Koinex))
-        plot_graphs(df)
+    # while True:
+
+    curr_time = time.currentTime()
+    url1 = Koinex_Get_data.Getdata("https://koinex.in/api/ticker")
+    url1.getdatafromurl()
+    finaldict = {curr_time: url1.convertdatafromurl().get('prices')}
+    Koinex.update(finaldict)
+    df = pd.read_json(json.dumps(Koinex))
+    plot_graphs(df)
+
 
 def plot_graphs(df):
     coins=['BTC','LTC','XRP']
@@ -25,21 +28,25 @@ def plot_graphs(df):
         graph = Plot_graph.Plot_graph(coin, df.transpose()[coin])
         graph.plot_graph()
     time.sleep(60)
+
 def frontend():
-    Koinex_GUI.Koinex_GUI().getCoin()
+    print "Entering FrontEnd"
+    Koinex_GUI.Koinex_GUI().main()
+
 def main():
+    while True:
+        backend_process = multiprocessing.Process(target=backend(), args=())
+        backend_process.start()
 
-    backend_process = multiprocessing.Process(target=backend(), args=())
-    backend_process.start()
+        gui_process = multiprocessing.Process(target=frontend(), args=())
+        gui_process.start()
 
-    gui_process = multiprocessing.Process(target=frontend(), args=())
-    gui_process.start()
+        backend_process.join()
+        gui_process.join()
 
-    backend_process.join()
-    gui_process.join()
+        print("whatever")
 
 
-    print("whatever")
 
 if __name__ == '__main__':
     main()
--- Koinex_GUI.py
+++ Koinex_GUI.py
@@ -1,18 +1,71 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 class Koinex_GUI:
-    def getCoin(self):
-        root = Tk()
-        root.title("First GUI")
-        root.geometry("560x560+0+0")
-        heading = Label(root, text="CrytoCurrency", font=("arial", 40, "bold"), fg="steelblue").pack()
+    coinPic = ""
 
-        label1 = Label(root, text="Enter the Name of the Coin", font=("arial", 20, "bold"), fg="black").place(x=10,y=200)
-        name = StringVar()
-        entry_box = Entry(root, textvariable=name, width=25, bg="white").place(x=380, y=210)
-        work = Button(root, text="Submit", width=15, height=5, bg="lightblue").place(x=250, y=300)
+    def bitCoin(self):
+        global coinPic
+        coinPic = "BTC.png"
+        print "Entered BitCoin"
+        # root.destroy()
+        self.imageWindow(coinPic, "Bitcoin")
+
+    def litCoin(self):
+        global coinPic
+        coinPic = "LTC.png"
+        print "Entered LiteCoin"
+        # root.destroy()
+        self.imageWindow(coinPic, "Litcoin")
+
+    def rippleCoin(self):
+        global coinPic
+        coinPic = "XRP.png"
+        print "Entered RippleCoin"
+        self.imageWindow(coinPic, "Ripplecoin")
+
+    def imageWindow(self, coinPic, name):
+        new = Toplevel()
+        new.title("Output")
+        new.geometry("760x760+0+0")
+        new.resizable(width=True, height=True)
+        label1 = Label(new, text="%s GRAPH" % name, font=("arial", 20, "bold"), fg="black")
+        label1.place(x=10, y=200)
+        label1.pack()
+        img = Image.open(coinPic)
+        img = img.resize((650, 650), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        panel = Label(new, image=img, width=600, height=600)
+        # panel.pack()
+        panel.place(x=50, y=50)
+        new.mainloop()
+
+    def main(self):
+        root = Tk()
+        root.title("CrytoCurrency")
+        root.geometry("560x260+0+0")
+        root.resizable(width=True, height=True)
+        Button(root, text="Bitcoin", width=15, height=3, bg="lightblue", command=self.bitCoin).place(x=30, y=100)
+        Button(root, text="Litcoin", width=15, height=3, bg="lightblue", command=self.litCoin).place(x=190, y=100)
+        Button(root, text="Ripple", width=15, height=3, bg="lightblue", command=self.rippleCoin).place(x=350, y=100)
         root.mainloop()
-        return name.get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
